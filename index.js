2f07c1ed66ee2938a949779c7dc58d395853e576
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-23 22:46:17 +0600

    Creating single house data related API done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx5eerd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,7 @@ async function run() {
 
     // Collection
     const housesCollection = client.db("houseHunterDB").collection("houses");
+    const roomSizesCollection = client.db("houseHunterDB").collection("foundByRoomSizes");
 
     // Getting all houses data
     app.get("/houses", async (req, res) => {
@@ -34,14 +35,22 @@ async function run() {
       res.send(result);
     });
 
+    // Getting single house data
+    app.get("/houses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await housesCollection.findOne(query);
+      res.send(result);
+    });
+
     // Getting room sizes data
-    app.get("/houses/:roomSize", async (req, res) => {
+    app.get("/roomSizes/:roomSize", async (req, res) => {
       if (
         req.params.roomSize == "Small" ||
         req.params.roomSize == "Medium" ||
         req.params.roomSize == "Large"
       ) {
-        const cursor = housesCollection.find({
+        const cursor = roomSizesCollection.find({
           roomSize: req.params.roomSize,
         });
         const result = await cursor.toArray();
