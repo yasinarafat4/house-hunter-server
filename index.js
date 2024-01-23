4b6744dff23bcb6adc73d512aa0516df232fb510
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-23 21:52:22 +0600

    Creating all houses data related API done

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx5eerd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,10 +25,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-// Collection
-const housesCollection = client.db("houseHunterDB").collection("houses");
+    // Collection
+    const housesCollection = client.db("houseHunterDB").collection("houses");
 
-
+    // Getting all houses data
+    // Users related API's
+    app.get("/hosues", async (req, res) => {
+      const result = await housesCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -42,9 +44,6 @@ const housesCollection = client.db("houseHunterDB").collection("houses");
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Server is running");
